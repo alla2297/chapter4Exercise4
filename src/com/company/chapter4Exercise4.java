db012f71940907f501cd8951b0378158fb3bff22
alla2297 <alla2297@edu.easj.dk>
2021-09-09 11:14:43 +0200

    Initial commit

---
+++ src/com/company/chapter4Exercise4.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main{
+
+    public static void main(String[] args) {
+        int test;
+        System.out.println(test);
+        //Value = Boo!7
+        System.out.println("Boo!"+7);
+    }
+}
+/*OpenJDK 64-Bit Server VM warning: Sharing is only supported for boot loader classes because bootstrap classpath has been appended
+ Error: Could not find or load main class com.company.Main
+Caused by: java.lang.ClassNotFoundException: com.company.Main
+*/
